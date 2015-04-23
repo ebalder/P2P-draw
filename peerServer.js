663b870948096ce7112821f8ff88734e9f8e5c0d
lexas <dislexas@gmail.com>
2015-04-22 23:22:38 -0600

    deployable trash

--- peerServer.js
+++ peerServer.js
@@ -2,3 +2,15 @@ var PeerServer = require('peer').PeerServer;
 var server = new PeerServer({ port: 9000 });
 
 console.log("P2P broker started.");
+
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var router = express.Router();
+
+var serveStatic = require('serve-static');
+
+app.use(serveStatic(__dirname + '/public'));
+
+server.listen(process.env.PORT);
