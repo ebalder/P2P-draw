d47d7100c7575706223406712aa887b8f348c677
lexas <dislexas@gmail.com>
2015-04-23 00:01:45 -0600

    add public/lib

--- peerServer.js
+++ peerServer.js
@@ -1,16 +1,20 @@
-var PeerServer = require('peer').PeerServer;
-var server = new PeerServer({ port: 9000 });
-
-console.log("P2P broker started.");
-
 
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+
+var peerServer = require('peer').ExpressPeerServer;
+
+
 var router = express.Router();
 
 var serveStatic = require('serve-static');
 
 app.use(serveStatic(__dirname + '/public'));
 
-server.listen(process.env.PORT);
+app.use('/peerServer', peerServer(server, {}));
+
+
+server.listen(process.env.PORT || 9000);
+
+console.log("P2P broker started.", process.env.PORT);
